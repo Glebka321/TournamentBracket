5924fddd4c30c513c3bee2396d4dd034903160ca
Gleb <tsyganov@wisc.edu>
2018-04-18 20:56:07 -0500

    Add generic return type

--- FileReader.java
+++ FileReader.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    List parseFile(String filePath) throws IOException {
+    List<String> parseFile(String filePath) throws IOException {
         Stream<String> stream = Files.lines(Paths.get(filePath));
         return stream.filter(line -> !line.isEmpty()).map(String::toUpperCase).map(String::trim)
                         .collect(Collectors.toList());
