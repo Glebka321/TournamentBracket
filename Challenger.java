6a66680dc536d7bd8932d62820aaa78a61acc0cd
Gleb <tsyganov@wisc.edu>
2018-04-18 20:35:05 -0500

    Modify all files

--- Challenger.java
+++ Challenger.java
@@ -1,5 +1,13 @@
-package application;
-
 public class Challenger {
-
+    String name;
+    int rank;
+    
+    public Challenger(String name, int rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+    
+    private String getName() {
+        return name;
+    }
 }
